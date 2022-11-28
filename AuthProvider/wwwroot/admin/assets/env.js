c5c2c30122356ed274f3a0fb228d1cf428900bba
Faizan-Zia <faizanzia005@gmail.com>
2022-11-28 11:48:04 +0500

    testing stuff

---
+++ AuthProvider/wwwroot/admin/assets/env.js
@@ -0,0 +1,8 @@
+var ENV = {
+    ApiUrl: 'https://ed-10370001.educative.run/',
+    UiUrl: 'https://ed-10370001.educative.run/',
+    AuthorityUrl: 'https://ed-10370001.educative.run/',   
+    AddUserPassword: true,
+    CommunityEdition: true,
+    UserManagementDisabled: false
+};
