5560bd104f8e61d56fce24a1096810b1967c552e
Faizan-Zia <faizanzia005@gmail.com>
2022-12-02 06:48:47 +0000

    combing server

--- AuthProvider/wwwroot/admin/assets/env.js
+++
@@ -1,8 +0,0 @@
-var ENV = {
-    ApiUrl: 'https://ed-10370001.educative.run/',
-    UiUrl: 'https://ed-10370001.educative.run/',
-    AuthorityUrl: 'https://ed-10370001.educative.run/',   
-    AddUserPassword: true,
-    CommunityEdition: true,
-    UserManagementDisabled: false
-};
---
+++ LearningSignalR/AuthProvider/wwwroot/admin/assets/env.js
@@ -0,0 +1,8 @@
+var ENV = {
+    ApiUrl: 'https://ed-10370001.educative.run:3000/',
+    UiUrl: 'https://ed-10370001.educative.run:3000/',
+    AuthorityUrl: 'https://ed-10370001.educative.run:3000/',   
+    AddUserPassword: true,
+    CommunityEdition: true,
+    UserManagementDisabled: false
+};
